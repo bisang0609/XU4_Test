34b922f5027767665505a5692957c0c58aa78e0f
bisang0609 <bisang0609@gmail.com>
2019-12-30 09:48:11 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Test XU4 SJVA & OMV 5 Setup
-echo "Sheel Test"
-#cd /tmp
-#apt install git
-
+apt update -y && apt -y upgrade && apt -y install armbian-config
+apt install -y python-pip python-setuptools libzbar-dev libzbar0
+apt install -y libxml2-dev libxslt-dev python-dev libssl-dev
+apt install -y libffi-dev libjpeg8-dev zlib1g-dev
+apt install -y python-lxml python-wheel python-wheel
